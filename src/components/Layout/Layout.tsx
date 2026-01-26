9706c9ef07b4eb3751c8ffec291e44e279ceda53
Magui2328 <lmaguette995@gmail.com>
2026-01-27 00:06:59 +0100

    Fix main padding and remove unused imports
    Adjust main padding and clean up commented code.

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,19 +1,27 @@
-import React from 'react'
-import Header from './Header'
+importimport Header from './Header'
 import Footer from './Footer'
+// import CookieConsent from '../CookieConsent'
 
 interface LayoutProps {
   children: React.ReactNode
 }
 
 export default function Layout({ children }: LayoutProps) {
+  // const [showCookieConsent, setShowCookieConsent] = useState(true)
+
   return (
     <div className="min-h-screen bg-pale-yellow">
       <Header />
-      <main className="pt-32">
+      <main className="pt-20">
         {children}
       </main>
       <Footer />
+      {/*
+      {showCookieConsent && (
+        <CookieConsent onAccept={() => setShowCookieConsent(false)} />
+      )}
+      */}
     </div>
   )
+
 }

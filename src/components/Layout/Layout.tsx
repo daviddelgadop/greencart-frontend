c95980adea2b046ba0b0d31da74cff79ccb95ed6
Magui2328 <lmaguette995@gmail.com>
2026-01-27 00:03:04 +0100

    Update Layout.tsx

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,27 +1,19 @@
 import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
-// import CookieConsent from '../CookieConsent'
 
 interface LayoutProps {
   children: React.ReactNode
 }
 
 export default function Layout({ children }: LayoutProps) {
-  // const [showCookieConsent, setShowCookieConsent] = useState(true)
-
   return (
     <div className="min-h-screen bg-pale-yellow">
       <Header />
-      <main className="pt-20">
+      <main className="pt-32">
         {children}
       </main>
       <Footer />
-      {/*
-      {showCookieConsent && (
-        <CookieConsent onAccept={() => setShowCookieConsent(false)} />
-      )}
-      */}
     </div>
   )
 }
